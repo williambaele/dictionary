a18f8bbdb76b603dbd26050fa8925b1fa028467d
William Baele <baelewilliam1999@gmail.com>
2023-03-16 14:50:16 +0100

    Fixed JS

--- app.js
+++ app.js
@@ -14,15 +14,25 @@ function getDefinition(search) {
     if (request.readyState === XMLHttpRequest.DONE){
       if (request.status === 200){
         let answer = request.response
-        let definition = answer.meanings[0].definitions[0].definition
+        let definition = answer[0].meanings[0].definitions[0].definition;
+        let type = answer[0].meanings[0].partOfSpeech;
+        let word = answer[0].word;
+        let phonetic = answer[0].phonetic
         console.log('API connected');
-        console.log(definition);
-      }
+        console.log(answer[0].meanings[0].partOfSpeech);
+        console.log(answer[0].meanings[0].definitions[0].definition);
+        document.querySelector("#definition").innerText = definition;
+        document.querySelector("#type").innerText = type;
+        document.querySelector("#word").innerText = word;
+        document.querySelector("#phonetic").innerText = phonetic;
+
+
+        }
       else {
         console.log('API not working');
       }
     }
-}
+  }
 }
 
 
@@ -32,7 +42,7 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   if(input.value == ''){
-     document.querySelector("#input").classList.add('border-red-600','border-8');
+     document.querySelector("#input").classList.add('border-red-500','border-8');
      console.log("fail");
   }
   else {
