83cb4606cedf7435fb3cd84368c0dcbe0711e388
William Baele <baelewilliam1999@gmail.com>
2023-03-16 14:18:49 +0100

    Fixed JS

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-console.log("Hi");
-const url = https://api.dictionaryapi.dev/api/v2/entries/en/;
-
 let form = document.querySelector('#form');
+let search = "procrastination";
+let input = document.querySelector('#search');
+getDefinition(search);
 
 /* Search function */
-function getDefinition() {
-
+function getDefinition(search) {
+  const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'+ search;
+  let request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    if (request.readyState === XMLHttpRequest.DONE){
+      if (request.status === 200){
+        let answer = request.response
+        let definition = answer.meanings[0].definitions[0].definition
+        console.log('API connected');
+        console.log(definition);
+      }
+      else {
+        console.log('API not working');
+      }
+    }
+}
 }
 
 
@@ -14,7 +31,7 @@ function getDefinition() {
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if(input.value === '' || input.value === null){
+  if(input.value == ''){
      document.querySelector("#input").classList.add('border-red-600','border-8');
      console.log("fail");
   }
@@ -22,6 +39,5 @@ form.addEventListener('submit', (e) => {
     let search = input.value;
     console.log('You searched: ' + search);
     getDefinition(search);
-
   }
 });
